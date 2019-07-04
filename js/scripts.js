f8a26ce31962c54a6fe45d986ddf995c6011e82f
DenisMariupol <denis.prozhivarov@gmail.com>
2019-07-04 17:39:29 +0300

    f1

---
+++ js/scripts.js
@@ -0,0 +1,266 @@
+function initMap() {
+
+	let mapDiv = document.getElementById('map'),
+		center = {
+			lat: -7.9307512,
+			lng: 112.6355576
+		};
+	
+	let map = new google.maps.Map(mapDiv, {
+		center: center,
+		zoom: 16,
+		// styles: [
+		// 			  {
+		// 			    "elementType": "geometry",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#f5f5f5"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "elementType": "labels.icon",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "visibility": "off"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "elementType": "labels.text.fill",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#616161"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "elementType": "labels.text.stroke",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#f5f5f5"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "administrative.land_parcel",
+		// 			    "elementType": "labels.text.fill",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#bdbdbd"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "poi",
+		// 			    "elementType": "geometry",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#eeeeee"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "poi",
+		// 			    "elementType": "labels.text.fill",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#757575"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "poi.park",
+		// 			    "elementType": "geometry",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#e5e5e5"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "poi.park",
+		// 			    "elementType": "labels.text.fill",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#9e9e9e"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "road",
+		// 			    "elementType": "geometry",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#ffffff"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "road.arterial",
+		// 			    "elementType": "labels.text.fill",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#757575"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "road.highway",
+		// 			    "elementType": "geometry",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#dadada"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "road.highway",
+		// 			    "elementType": "labels.text.fill",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#616161"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "road.local",
+		// 			    "elementType": "labels.text.fill",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#9e9e9e"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "transit.line",
+		// 			    "elementType": "geometry",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#e5e5e5"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "transit.station",
+		// 			    "elementType": "geometry",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#eeeeee"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "water",
+		// 			    "elementType": "geometry",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#c9c9c9"
+		// 			      }
+		// 			    ]
+		// 			  },
+		// 			  {
+		// 			    "featureType": "water",
+		// 			    "elementType": "labels.text.fill",
+		// 			    "stylers": [
+		// 			      {
+		// 			        "color": "#9e9e9e"
+		// 			      }
+		// 			    ]
+		// 			  }
+		// 			]
+
+	});
+	let marker = new google.maps.Marker({
+		position : center,
+		map      : map,
+		icon     : "./img/pin.png"
+	});
+
+}
+
+$(document).ready(function(){
+	$('.slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 4000
+	});
+
+	$('.slidert').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  infinite: true,
+	  dots: false,
+	  responsive: [
+    {
+      breakpoint: 1260,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 610,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+	  // autoplay: true,
+	  // autoplaySpeed: 2000
+	  // variableWidth: true
+	});
+	// $('.slider').slick({
+	// 	infinite: true,
+	// 	slidesToShow: 1,
+ //        slidesToScroll: 1,
+	// 	dots: true,
+	// 	fade: true,
+	// 	arrows: false,
+	// 	autoplay: true,
+	// 	autoplaySpeed: 1000
+	// });
+	// $('.ln__slider').slick({
+	// 	infinite: true,
+	// 	slidesToShow: 3,
+	// 	slidesToScroll: 1,
+	// 	dots: true,
+	// 	autoplay: true,
+	// 	autoplaySpeed: 4000
+	// 	// fade: true
+	// });
+});
+
+(function(){
+	'use strict';
+
+	let toggler = document.querySelector('.toggler');
+	toggler.addEventListener('click', function(e){
+		e.preventDefault();
+		let nav = document.querySelector('.menu__items');
+		nav.classList.toggle('opened');
+	})
+
+	let links = document.querySelectorAll('.menu__link');
+	for (var i = 0; i < links.length; i++) {
+		let link = links[i];
+		link.addEventListener('click', function() {
+			setTimeout(function(){
+				let nav = document.querySelector('.menu__items');
+				nav.classList.remove('opened');
+			}, 300);
+		})
+	}
+
+
+})();
