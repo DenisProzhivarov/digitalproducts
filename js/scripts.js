aac6957d7bc57c4f034d5f2d22456f3737933140
DenisMariupol <denis.prozhivarov@gmail.com>
2019-07-05 14:40:01 +0300

    f5

--- js/scripts.js
+++ js/scripts.js
@@ -8,168 +8,7 @@ function initMap() {
 	
 	let map = new google.maps.Map(mapDiv, {
 		center: center,
-		zoom: 16,
-		// styles: [
-		// 			  {
-		// 			    "elementType": "geometry",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#f5f5f5"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "elementType": "labels.icon",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "visibility": "off"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "elementType": "labels.text.fill",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#616161"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "elementType": "labels.text.stroke",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#f5f5f5"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "administrative.land_parcel",
-		// 			    "elementType": "labels.text.fill",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#bdbdbd"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "poi",
-		// 			    "elementType": "geometry",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#eeeeee"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "poi",
-		// 			    "elementType": "labels.text.fill",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#757575"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "poi.park",
-		// 			    "elementType": "geometry",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#e5e5e5"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "poi.park",
-		// 			    "elementType": "labels.text.fill",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#9e9e9e"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "road",
-		// 			    "elementType": "geometry",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#ffffff"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "road.arterial",
-		// 			    "elementType": "labels.text.fill",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#757575"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "road.highway",
-		// 			    "elementType": "geometry",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#dadada"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "road.highway",
-		// 			    "elementType": "labels.text.fill",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#616161"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "road.local",
-		// 			    "elementType": "labels.text.fill",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#9e9e9e"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "transit.line",
-		// 			    "elementType": "geometry",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#e5e5e5"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "transit.station",
-		// 			    "elementType": "geometry",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#eeeeee"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "water",
-		// 			    "elementType": "geometry",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#c9c9c9"
-		// 			      }
-		// 			    ]
-		// 			  },
-		// 			  {
-		// 			    "featureType": "water",
-		// 			    "elementType": "labels.text.fill",
-		// 			    "stylers": [
-		// 			      {
-		// 			        "color": "#9e9e9e"
-		// 			      }
-		// 			    ]
-		// 			  }
-		// 			]
-
+		zoom: 16
 	});
 	let marker = new google.maps.Marker({
 		position : center,
@@ -193,10 +32,11 @@ $(document).ready(function(){
 	  slidesToShow: 3,
 	  slidesToScroll: 1,
 	  infinite: true,
+	  autoplay: true,
 	  dots: false,
 	  responsive: [
     {
-      breakpoint: 1260,
+      breakpoint: 710,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
@@ -204,10 +44,12 @@ $(document).ready(function(){
       }
     },
     {
-      breakpoint: 610,
+      breakpoint: 523,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        centerMode: true,
+        variableWidth: true
       }
     },
     
